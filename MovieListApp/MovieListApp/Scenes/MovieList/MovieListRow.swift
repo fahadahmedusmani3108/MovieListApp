8558ed135d1cc68542c815a7e20e069b387bc99f
Fahad <fahad.ahmed@avanzasolutions.com>
2022-12-28 15:52:57 +0500

    Added MovieListRow

---
+++ MovieListApp/MovieListApp/Scenes/MovieList/MovieListRow.swift
@@ -0,0 +1,22 @@
+//
+//  MovieListRow.swift
+//  MovieListApp
+//
+//  Created by Fahad Ahmed Usmani on 28/12/2022.
+//
+
+import SwiftUI
+
+struct MovieListRow: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+}
+
+struct MovieListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListRow()
+    }
+}
