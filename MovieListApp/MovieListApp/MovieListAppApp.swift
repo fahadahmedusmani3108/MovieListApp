b21330b39c17c3257fd8cc6eba0af244b3657408
Fahad <fahad.ahmed@avanzasolutions.com>
2022-12-28 15:31:09 +0500

    Initial Project Created

---
+++ MovieListApp/MovieListApp/MovieListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListAppApp.swift
+//  MovieListApp
+//
+//  Created by Fahad Ahmed Usmani on 28/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
