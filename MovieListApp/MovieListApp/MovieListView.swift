26d4f13c709df9537f16bc14efa20bc9959b0de4
Fahad <fahad.ahmed@avanzasolutions.com>
2022-12-28 15:44:45 +0500

    Added MovieListView

---
+++ MovieListApp/MovieListApp/MovieListView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieListView.swift
+//  MovieListApp
+//
+//  Created by Fahad Ahmed Usmani on 28/12/2022.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListView()
+    }
+}
